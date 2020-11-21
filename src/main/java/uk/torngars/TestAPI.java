d69f6ff762c3062d4b190fd5f244217c07e41b7d
Emad Davis <emaddavis@gmail.com>
2020-11-21 02:35:44 -0600

    Initial commit

---
+++ src/main/java/uk/torngars/TestAPI.java
@@ -0,0 +1,14 @@
+package uk.torngars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAPI.class, args);
+
+	}
+
+}
